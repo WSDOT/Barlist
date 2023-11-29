20b1b4c7afcc44e6aa477c2832bf42d617c2552c
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2023-11-29 12:45:17 -0800

    Mantis 667 - More fixes related to build-release process

--- Documentation/BuildDocumentation.bat
+++ Documentation/BuildDocumentation.bat
@@ -17,5 +17,5 @@ echo ^</script^> >> %SEARCH_HTML%
 %ARPDIR%\BridgeLink\RegFreeCOM\x64\Release\MakeDocMap Barlist
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\Barlist\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\Barlist\%1\
-copy %ARPDIR%\Barlist\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\Barlist\%1\
+xcopy /s /y /d %ARPDIR%\Barlist\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\Barlist\%1\
 copy %ARPDIR%\Barlist\Documentation\Barlist.dm %ARPDIR%\BridgeLink\Docs\Barlist\%1\
